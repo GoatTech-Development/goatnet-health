3948183511e6785d0464cf68b385eae761e46e7c
Neal Hitzfield <34891912+nhitz@users.noreply.github.com>
2024-04-03 12:36:23 -0700

    working smoothiechart

--- frontend/src/components/PingLatencyChart.vue
+++ frontend/src/components/PingLatencyChart.vue
@@ -1,131 +1,56 @@
 <template>
-  <Chart ref="primeChart" type="line" :data="chartData" :options="chartOptions" class="h-30rem"/>
+  <canvas id="mycanvas" width="500" height="200"></canvas>
 </template>
 
-<script setup>
-import 'chartjs-adapter-date-fns';
-import Chart from 'primevue/chart';
-import {onMounted, ref} from "vue";
+<script>
+import {onBeforeUnmount, onMounted, ref} from 'vue';
+import {SmoothieChart, TimeSeries} from 'smoothie';
 import {setupWebSocket} from "@/services/websocketService.js";
 
-// Setup websocket connection
-const ws = ref(null);
+export default {
+  setup() {
+    const smoothie = ref(null);
+    const line1 = ref(null);
+    const line2 = ref(null);
+    let interval = null;
+    let ws = null;
 
-onMounted(async () => {
-  ws.value = setupWebSocket(updateChartData);
-});
+    onMounted(() => {
+      smoothie.value = new SmoothieChart({
+        minValueScale: 1.1,
+        maxValueScale: 1.1,
+        grid: {
+          strokeStyle: 'rgb(125, 0, 0)', fillStyle: 'rgb(60, 0, 0)',
+          lineWidth: 1, millisPerLine: 250, verticalSections: 6,
+        },
+        labels: {fillStyle: 'rgb(60, 0, 0)'}
+      });
 
-function getGradient(chart) {
-  const ctx = chart.ctx;
-  const gradient = ctx.createLinearGradient(0, 0, 0, chart.height);
-  gradient.addColorStop(0, 'rgba(255, 105, 180, 0.5)'); // Start color
-  gradient.addColorStop(1, 'rgba(255, 192, 203, 0)'); // End color
-  return gradient;
-}
+      line1.value = new TimeSeries();
+      // line2.value = new TimeSeries();
 
-let chartData = {
-  labels: [], // Empty array for labels
-  datasets: [
-    {
-      label: 'Latency',
-      data: [], // Empty array for data
-      fill: true,
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.4,
-      backgroundColor: (context) => getGradient(context.chart)
-    }
-  ]
-};
+      // interval = setInterval(function () {
+      //   line2.value.append(Date.now(), Math.random());
+      // }, 1000);
 
-const chartOptions = {
-  responsive: true,
-  animation: {
-    duration: 100, // Duration of the animation in milliseconds
-    easing: 'linear', // Easing function to use
-  },
-  legend: {
-    display: true,
-    position: 'bottom',
-    labels: {
-      fontColor: '#3300CC'
-    }
-  },
-  tooltips: {
-    enabled: true,
-    backgroundColor: '#FFFFFF',
-    titleFontColor: '#000000',
-    bodyFontColor: '#000000'
-  },
-  elements: {
-    point: {
-      // Hide points on chart unless mouse-over
-      radius: 0,
-      hitRadius: 100,
-    },
-  },
-  scales: {
-    x: {
-      title: {
-        display: true,
-        text: 'Time',
-        font: {
-          size: 16,
-          weight: 'bold'
-        }
-      },
-      type: 'time',
-      time: {
-        unit: 'second'
-      },
-      ticks: {
-        source: 'data',
-        color: '#FFFFFF'
-      },
-      animation: {
-        duration: 1000, // new duration for the x-axis transition
-      },
-    },
-    y: {
-      beginAtZero: true,
-      title: {
-        display: true,
-        text: 'Latency (ms)',
-        font: {
-          size: 16,
-          weight: 'bold'
-        }
-      },
-      ticks: {
-        color: '#FFFFFF'
+      ws = setupWebSocket((latency) => {
+        line1.value.append(Date.now(), latency);
+      });
+
+      smoothie.value.addTimeSeries(line1.value,
+          {strokeStyle: 'rgb(0, 255, 0)', fillStyle: 'rgba(0, 255, 0, 0.4)', lineWidth: 3});
+      // smoothie.value.addTimeSeries(line2.value,
+      //     {strokeStyle: 'rgb(255, 0, 255)', fillStyle: 'rgba(255, 0, 255, 0.3)', lineWidth: 3});
+
+      smoothie.value.streamTo(document.getElementById("mycanvas"), 5000 /*delay*/);
+    });
+
+    onBeforeUnmount(() => {
+      clearInterval(interval);
+      if (ws && ws.ws) {
+        ws.ws.close();
       }
-    },
-  },
-};
-
-const primeChart = ref();
-
-const windowSize = 8; // Size of the sliding window
-
-let latencies = []; // Array to store the last N latencies
-
-const updateChartData = (latency) => {
-  // Add the new latency to the end of the array
-  latencies.push(latency);
-
-  // If the array size exceeds the window size, remove the oldest latency from the start of the array
-  if (latencies.length > windowSize) {
-    latencies.shift();
+    });
   }
-
-  // Update the chart data and labels
-  chartData.datasets[0].data = [...latencies];
-  chartData.labels = latencies.map((_, i) => new Date(Date.now() - (windowSize - i - 1) * 1000));
-};
-
-// Update the chart data at a set interval
-setInterval(() => {
-  if (primeChart.value !== null && primeChart.value !== undefined) {
-    primeChart.value.refresh();
-  }
-}, 1000); // Adjust the interval as needed// Adjust the interval as needed
+}
 </script>
